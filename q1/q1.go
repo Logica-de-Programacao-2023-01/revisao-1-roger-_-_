3034cbf8cb1bfabdebbdbbe81102b533568cc53d
ItzTutu <104566356+tutupro@users.noreply.github.com>
2023-04-18 10:22:41 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -2,31 +2,39 @@ package q1
 
 import "fmt"
 
+// Uma loja de roupas precisa de uma função para verificar
+//se um cliente é elegível para um desconto com base
+//em seu histórico de compras.
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
 	if currentPurchase == 0 {
-		fmt.Print("valor da compra inválido")
+		return 0, nil
+		fmt.Errorf("valor da compra inválido")
 	}
+
 	var soma, media, resultado float64
+	
+
 	for i := 0; i < len(purchaseHistory); i++ {
 		soma += purchaseHistory[i]
 	}
+
 	media = soma / float64(len(purchaseHistory))
+
 	if len(purchaseHistory) == 0 {
 		resultado = currentPurchase * 0.9
-	} else {
-		if soma > 1000.00 {
-			resultado = currentPurchase * 0.9
-		}
-		if soma <= 1000.00 {
-			resultado = currentPurchase * 0.95
-		}
-		if soma <= 500.00 {
-			resultado = currentPurchase * 0.98
-		}
-		if media > 1000.00 {
-			resultado = currentPurchase * 0.8
-		}
 	}
+	if soma > 1000 {
+		resultado = currentPurchase * 0.1
+	} else if soma <= 1000 {
+		resultado = currentPurchase * 0.05
+	} else if soma <= 500 {
+		resultado = currentPurchase * 0.02
+	}
+	if media > 1000 {
+		resultado = currentPurchase * 0.2
+	}
+
 	return resultado, nil
 }
 
