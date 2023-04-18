05abfb001c6e9a1d91a3e13d3c3676ad2cd0b7e8
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-04-18 15:34:45 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,27 @@
 package q2
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if len(text)-int(strings.Count(text, " ")) == 0 {
+		return 0, fmt.Errorf("frase inexistente")
+	}
+	partes := strings.Split(text, " ")
+	soma := 0.0
+	contador := 0.0
+	media := 0.0
+	for i := 0; i < len(partes); i++ {
+		if len(partes[i]) >= 2 {
+			fmt.Println(partes[i])
+			soma += float64(len(partes[i]))
+			contador++
+		}
+	}
+	media = soma / contador
+	return media, nil
 }
