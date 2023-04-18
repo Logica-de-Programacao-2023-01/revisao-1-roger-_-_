5e8a7b84492a652db57bf90bfcf39f94c8ed979e
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-04-18 10:19:00 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,32 @@
 package q1
 
+import "fmt"
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if currentPurchase == 0 {
+		fmt.Print("valor da compra inválido")
+	}
+	var soma, media, resultado float64
+	for i := 0; i < len(purchaseHistory); i++ {
+		soma += purchaseHistory[i]
+	}
+	media = soma / float64(len(purchaseHistory))
+	if len(purchaseHistory) == 0 {
+		resultado = currentPurchase * 0.9
+	} else {
+		if soma > 1000.00 {
+			resultado = currentPurchase * 0.9
+		}
+		if soma <= 1000.00 {
+			resultado = currentPurchase * 0.95
+		}
+		if soma <= 500.00 {
+			resultado = currentPurchase * 0.98
+		}
+		if media > 1000.00 {
+			resultado = currentPurchase * 0.8
+		}
+	}
+	return resultado, nil
 }
+
